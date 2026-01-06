4eca853c357412d6bf8e0fb2cf6069670b6a2440
Kevin Boutilier <Kevin.Boutilier@gmail.com>
2026-01-06 02:36:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8890386089-d1a4e",
+  "appId": "1:474295895468:web:6275cfb1ed85dd1d962609",
+  "apiKey": "AIzaSyD6YGSEuCmmIu5HNBr0GHB6lcKzrVKCwf0",
+  "authDomain": "studio-8890386089-d1a4e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "474295895468"
+};
