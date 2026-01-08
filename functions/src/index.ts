1ed0e9ed8b76e14cdc5776fa054975dec7a3482b
Nivekkb <kevin.boutilier@gmail.com>
2026-01-08 16:11:20 -0500

    functions/src/index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,9 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {setGlobalOptions} from "firebase-functions";
-import {onRequest} from "firebase-functions/https";
-import * as logger from "firebase-functions/logger";
+import { setGlobalOptions } from "firebase-functions";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -27,6 +25,6 @@ import * as logger from "firebase-functions/logger";
 setGlobalOptions({ maxInstances: 10 });
 
 // export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
+//   logger.info("Hello logs!", { structuredData: true });
 //   response.send("Hello from Firebase!");
 // });
